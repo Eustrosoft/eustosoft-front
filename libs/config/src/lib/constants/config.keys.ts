61cde4814fb99d9de405e61ff1100c14dee6bed5
Ilya <riper752@gmail.com>
2023-05-18 11:30:06 +0400

    Backup config fetch functionality (if fails to fetch from first url - tries second)

--- libs/config/src/lib/constants/config.keys.ts
+++ libs/config/src/lib/constants/config.keys.ts
@@ -3,4 +3,3 @@ import { ConfigKey } from '../interfaces/config.interface';
 export const explorerUrlKey: ConfigKey = 'explorerUrl';
 export const dispatcherUrlKey: ConfigKey = 'dispatcherUrl';
 export const loginUrlKey: ConfigKey = 'loginUrl';
-export const appsPageUrlKey: ConfigKey = 'appsPageUrl';
--- libs/config/src/lib/services/config.service.ts
+++ libs/config/src/lib/services/config.service.ts
@@ -1,6 +1,12 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, shareReplay } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  catchError,
+  Observable,
+  shareReplay,
+  switchMap,
+  throwError,
+} from 'rxjs';
 import { Config } from '../interfaces/config.interface';
 import { APP_BASE_HREF } from '@angular/common';
 
@@ -11,8 +17,31 @@ export class ConfigService {
   private configUrl = `${window.location.origin}${
     this.appBaseHref
   }config.json?${Date.now()}`;
+  private backupConfigUrl = `${
+    window.location.origin
+  }/config.json?${Date.now()}`;
 
+  private mainConfig = this.http
+    .get<Config>(this.configUrl)
+    .pipe(switchMap(() => throwError(() => '')));
+  private backupConfig = this.http.get<Config>(this.backupConfigUrl);
   getConfig(): Observable<Config> {
-    return this.http.get<Config>(this.configUrl).pipe(shareReplay(1));
+    return this.mainConfig.pipe(
+      catchError((err: HttpErrorResponse) => {
+        console.error('Failed to fetch configuration from the first URL:', err);
+        return this.backupConfig.pipe(
+          catchError((err: HttpErrorResponse) => {
+            console.error(
+              'Failed to fetch configuration from the second URL:',
+              err
+            );
+            return throwError(
+              () => `Unable to fetch configuration from ${err.url}`
+            );
+          })
+        );
+      }),
+      shareReplay(1)
+    );
   }
 }
