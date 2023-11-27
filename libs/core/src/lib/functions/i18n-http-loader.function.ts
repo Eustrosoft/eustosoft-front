08842f172ad85f01978868aad950a98199cfd82d
Ilya <riper752@gmail.com>
2023-11-27 20:52:47 +0400

    Prevent caching of localization packages

--- libs/core/src/lib/functions/i18n-http-loader.function.ts
+++ libs/core/src/lib/functions/i18n-http-loader.function.ts
@@ -9,5 +9,9 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(
+    http,
+    './assets/i18n/',
+    `.json?v=${Date.now()}`
+  );
 }
