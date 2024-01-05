8a338625a8a7812b507df8e7da7cbf3ff2646fb7
Ilya <riper752@gmail.com>
2024-01-05 18:02:04 +0400

    Configuration and test of husky + lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024. IdrisovII & EustroSoft.org
+ *
+ * This file is part of eustrosoft-front project.
+ * See the LICENSE file at the project root for licensing information.
+ */
+
+module.exports = {
+  '{apps,libs,tools}/**/*.{ts,tsx}': (files) => {
+    return `nx affected --target=typecheck --files=${files.join(',')}`;
+  },
+  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': [
+    (files) => `nx affected:lint --files=${files.join(',')}`,
+    (files) => `nx format:write --files=${files.join(',')}`,
+  ],
+};
